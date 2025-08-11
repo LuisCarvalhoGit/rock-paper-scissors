d1ae20c1ef9e8674ee1153575cd045b8d3c12689
Luis Carvalho <pro.luis.carvalho@gmail.com>
2025-08-11 11:04:24 +0100

    rock-paper-scissors-UI

--- javascript.js
+++ javascript.js
@@ -2,12 +2,7 @@ function getComputerChoice() {
     return (Math.random() * 100);
 }
 
-function getHumanChoice() {
-
-    return prompt("Choose: ROCK | PAPER | SCISSORS").toLowerCase()
-}
-
-function playRound(computernum, humanchoice) {
+function playRound(computernum, humanchoice, maxPoints) {
 
     let computerchoice;
     if(computernum < 33) {
@@ -24,41 +19,47 @@ function playRound(computernum, humanchoice) {
         (computerchoice === "scissors" && humanchoice === "paper")) 
         {
             computerScore++
-            console.log(`Computer Wins! ${computerchoice} beats ${humanchoice} | Human: ${humanScore} points vs Computer: ${computerScore}`)
+            results.textContent = `Computer Wins! ${computerchoice} beats ${humanchoice} | Human: ${humanScore} points vs Computer: ${computerScore}`
         }
     else if ((humanchoice === "rock" && computerchoice === "scissors") ||
              (humanchoice === "paper" && computerchoice === "rock") ||
              (humanchoice === "scissors" && computerchoice === "paper"))
              {
                 humanScore++
-                console.log(`Human Wins! ${humanchoice} beats ${computerchoice} | Human: ${humanScore} points vs Computer: ${computerScore}`)
+                results.textContent = `Human Wins! ${humanchoice} beats ${computerchoice} | Human: ${humanScore} points vs Computer: ${computerScore}`
              }
     else {
-        console.log(`It's a Tie! | Human: ${humanScore} points vs Computer: ${computerScore}`)
+        results.textContent = `It's a Tie! | Human: ${humanScore} points vs Computer: ${computerScore}`
     }
 
+
+    if(computerScore === maxPoints && computerScore > humanScore) {
+        results.textContent = "Computer Wins!"
+        return
+    } else if (humanScore === maxPoints && humanScore > computerScore) {
+        results.textContent = "Human Wins!"
+        return
+    }
     
 }
 
-function playGame(numRounds) {
 
-    // play till 5 points
-    while(humanScore < numRounds && computerScore < numRounds) {
-
-        playRound(getComputerChoice(), getHumanChoice())
-    }
-
-    if(humanScore > computerScore) {
-        console.log("Human Wins!")
-    } else {
-        console.log("Computer Wins!")
-    }
-}
 
 let computerScore = 0;
 let humanScore = 0;
+let maxPoints = 5
 
-playGame(5);
+let btnRock = document.getElementById("btnRock")
+let btnPaper = document.getElementById("btnPaper")
+let btnScissors = document.getElementById("btnScissors")
+let results = document.getElementById("results")
+
+
+btnRock.addEventListener("click", () => playRound(getComputerChoice(), "rock", maxPoints))
+btnPaper.addEventListener("click", () => playRound(getComputerChoice(), "paper", maxPoints))
+btnScissors.addEventListener("click", () => playRound(getComputerChoice(), "scissors", maxPoints))
+
+
 
 
 
